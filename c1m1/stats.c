a60dbaa3b9d2e35ec57766e17aa6151bc49aae6a
gabman15 <gabrielmcninch@gmail.com>
2024-03-11 19:51:02 -0400

    Revert "Implemented all functions and created a Makefile for compilation"
    This reverts commit 774183de7f7fca74db6a9945bb15eed121c2e97a.

--- c1m1/stats.c
+++ c1m1/stats.c
@@ -30,108 +30,43 @@
 
 void main() {
 
-    unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-    /* Other Variable Declarations Go Here */
-    /* Statistics and Printing Functions Go Here */
-
-    printf("Unsorted Array:\n");
-    print_array(test,SIZE);
-    printf("\nSorting Array...\n\n");
-    sort_array(test,SIZE);
-    printf("Sorted Array:\n");
-    print_array(test,SIZE);
-    printf("\n");
-    print_statistics(test, SIZE);
+  /* Other Variable Declarations Go Here */
+  /* Statistics and Printing Functions Go Here */
+  print_statistics(test, SIZE);
 }
 
 void print_statistics(unsigned char *arr, unsigned int len) {
-    unsigned char median = find_median(arr, len);
-    unsigned char mean = find_mean(arr, len);
-    unsigned char max = find_maximum(arr, len);
-    unsigned char min = find_minimum(arr, len);
 
-    printf("Statistics:\nMedian: %d\nMean: %d\nMaximum: %d\nMinimum: %d\n",
-           median, mean, max, min);
 }
 
 void print_array(unsigned char *arr, unsigned int len) {
-    int i;
-    for (i = 0; i < len; i++) {
-        if (i == len-1)
-            printf("%d",arr[i]);
-        else {
-            printf("%d, ",arr[i]);
-            if (i % 8 == 0 && i != 0)
-                printf("\n");
-        }
-    }
-    printf("\n");
+
 }
 
 unsigned char find_median(unsigned char *arr, unsigned int len) {
-    if (len == 1)
-        return arr[0];
-    if (len % 2 == 0) { //even
-        int middle = len / 2;
-        // return mean of the two middle numbers
-        return ((arr[middle - 1] + arr[middle]) / 2);
-    } else //odd
-        return arr[len / 2];
+
 }
 
 unsigned char find_mean(unsigned char *arr, unsigned int len) {
-    unsigned int sum = 0;
-    int i;
-    for (i = 0; i < len; i++) {
-        sum += arr[i];
-    }
-    
-    return (unsigned char) (sum / len);
+
 }
 
 unsigned char find_maximum(unsigned char *arr, unsigned int len) {
-    unsigned char max = arr[0];
-    int i;
-    for (i = 1; i < len; i++) {
-        if (arr[i] > max)
-            max = arr[i];
-    }
-    return max;
+
 }
 
 unsigned char find_minimum(unsigned char *arr, unsigned int len) {
-    unsigned char min = arr[0];
-    int i;
-    for (i = 1; i < len; i++) {
-        if (arr[i] < min)
-            min = arr[i];
-    }
-    return min;
-}
 
-void swap (unsigned char *first, unsigned char *second) {
-    int temp = *first;
-    *first = *second;
-    *second = temp;
 }
 
 void sort_array(unsigned char *arr, unsigned int len) {
-    int i, j, max_index;
 
-    for (i = 0; i < len - 1; i++) {
-        max_index = i;
-        for (j = i + 1; j < len; j++) {
-            if (arr[j] > arr[max_index])
-                max_index = j;
-        }
-        if (max_index != i)
-            swap(&arr[max_index], &arr[i]);
-    }
 }
 
 
